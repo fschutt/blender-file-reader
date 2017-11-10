4473507f8dacd8f634449af371332338e405382e
Felix Schütt <felix.schuett@maps4print.com>
2017-11-10 03:14:06 +0100

    Added string-based code generation, fixed open curly brace issue

--- blender-derive/src/lib.rs
+++ blender-derive/src/lib.rs
@@ -7,6 +7,9 @@ extern crate quote;
 
 use proc_macro::TokenStream;
 
+extern crate byteorder;
+use byteorder::{BigEndian, ReadBytesExt, LittleEndian, WriteBytesExt};
+
 #[proc_macro_derive(BlenderRead)]
 pub fn blender_read(input: TokenStream) -> TokenStream {
     let s = input.to_string();
@@ -24,44 +27,86 @@ pub fn blender_write(input: TokenStream) -> TokenStream {
 }
 
 fn impl_blender_read(ast: &syn::DeriveInput) -> quote::Tokens {
-
+    use syn::{Body, VariantData};
+    let mut output_tokens = quote::Tokens::new();
     let name = &ast.ident;
-    quote! {
-        impl BlenderRead for #name {
-            fn read_big_endian<R: ::std::io::Read>(mut buffer: R) -> Self {
-                // TODO: use byteorder!
-                let mut blender_data: #name = unsafe { ::std::mem::zeroed() };
-                let blender_data_size = ::std::mem::size_of::<#name>();
+    // buffer.read_u16::<BigEndian>().unwrap()
+    if let Body::Struct(VariantData::Struct(ref fields)) = ast.body {
 
-                unsafe {
-                    let blender_data_slice = ::std::slice::from_raw_parts_mut(
-                        &mut blender_data as *mut _ as *mut u8,
-                        blender_data_size
-                    );
-                    buffer.read_exact(blender_data_slice).unwrap();
-                }
-
-                blender_data
-            }
-
-            fn read_little_endian<R: ::std::io::Read>(mut buffer: R) -> Self {
-                // TODO: use byteorder!
-                let mut blender_data: #name = unsafe { ::std::mem::zeroed() };
-                let blender_data_size = ::std::mem::size_of::<#name>();
-
-                unsafe {
-                    let blender_data_slice = ::std::slice::from_raw_parts_mut(
-                        &mut blender_data as *mut _ as *mut u8,
-                        blender_data_size
-                    );
-                    buffer.read_exact(blender_data_slice).unwrap();
-                }
-
-                blender_data
-            }
+        // implement big endian
+        output_tokens.append(quote! { impl #name });
+        output_tokens.append("{");
+        output_tokens.append("fn read_big_endian<R: ::std::io::Read>(mut buffer: R) -> Self");
+        output_tokens.append("{");
+        output_tokens.append(quote! { 
+            let mut blender_data: #name = unsafe { ::std::mem::zeroed() };
+        });
+/*
+        for field in fields.iter() {
+            let ident = &field.ident;
+            let ty = &field.ty;
+            output_tokens.append(quote! {
+                blender_data.#field
+            });
+            output_tokens.append(" = ");
+            output_tokens.append(quote! { buffer.read_#ty::<BigEndian>().unwrap(); });
+            /*
+            match ty {
+                Array(a) => {
+                    //             output_tokens.append("");
+                },
+                
+            }*/
         }
+*/      
+        output_tokens.append(quote! { 
+            let blender_data_size = ::std::mem::size_of::<#name>();
+            
+            unsafe {
+                let blender_data_slice = ::std::slice::from_raw_parts_mut(
+                    &mut blender_data as *mut _ as *mut u8,
+                    blender_data_size
+                );
+                buffer.read_exact(blender_data_slice).unwrap();
+            }
+            
+            blender_data
+        });
+        
+        output_tokens.append("}");
+        output_tokens.append("}");
+
+        output_tokens.append(quote! {
+            impl BlenderRead for #name { 
+                fn read_big_endian<R: ::std::io::Read>(mut buffer: R) -> Self {
+                    #name::read_big_endian(buffer)
+                }
+
+                fn read_little_endian<R: ::std::io::Read>(mut buffer: R) -> Self {
+                    // TODO: use byteorder!
+                    let mut blender_data: #name = unsafe { ::std::mem::zeroed() };
+                    let blender_data_size = ::std::mem::size_of::<#name>();
+
+                    unsafe {
+                        let blender_data_slice = ::std::slice::from_raw_parts_mut(
+                            &mut blender_data as *mut _ as *mut u8,
+                            blender_data_size
+                        );
+                        buffer.read_exact(blender_data_slice).unwrap();
+                    }
+
+                    blender_data
+                }                    
+            }
+        });
+        
+    } else {
+        panic!("#[derive(BlenderRead)] is only defined for structs");
     }
+
+    output_tokens
 }
+
 
 fn impl_blender_write(ast: &syn::DeriveInput) -> quote::Tokens {
     let name = &ast.ident;
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ extern crate blender_derive;
 #[repr(C, packed)]
 #[derive(Debug, BlenderRead, BlenderWrite)]
 struct Configuration {
-   item1: u8,
-   item2: u16,
-   item3: i32,
-   item4: [char; 8]
+   header: [char; 7],
+   pointer_size: char,
+   endianness: char,
+   version: [char; 3],
 }
 
 trait BlenderRead {
@@ -20,15 +20,8 @@ trait BlenderWrite {
     fn write_little_endian<W: ::std::io::Write>(&self, target: W) -> Result<(), ::std::io::Error>;
 }
 
-const CONFIG_DATA: &'static [u8] = &[
-  0xfd, 0xb4, 0x50, 0x45, 0xcd, 0x3c, 0x15, 0x71, 0x3c, 0x87, 0xff, 0xe8,
-  0x5d, 0x20, 0xe7, 0x5f, 0x38, 0x05, 0x4a, 0xc4, 0x58, 0x8f, 0xdc, 0x67,
-  0x1d, 0xb4, 0x64, 0xf2, 0xc5, 0x2c, 0x15, 0xd8, 0x9a, 0xae, 0x23, 0x7d,
-  0xce, 0x4b, 0xeb
-];
-
 fn main() {
-    let mut config = CONFIG_DATA;
-    let data = Configuration::read_big_endian(&mut config);
+    let mut test_file = ::std::fs::File::open("assets/default_cube.blend").unwrap();
+    let data = Configuration::read_big_endian(&mut test_file);
     println!("Read structure: {:#?}", data);
 }
